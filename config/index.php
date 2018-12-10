928816f51692f6278f2452e89a87f72dcc7cfbc3
Farrel Franqois <farrel@franqois.id>
2018-12-11 01:21:00 +0700

    Agar Error 403 bisa berjalan pada PHP versi lama.

--- config/index.php
+++ config/index.php
@@ -1,3 +1,7 @@
 <?php
-  http_response_code(403);
+  if (!function_exists('http_response_code')) {
+    header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden"); 
+  } else {
+    http_response_code(403);
+  }
 ?>
