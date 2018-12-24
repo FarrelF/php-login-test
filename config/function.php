e26fb93683eb014ad16ecc74239339956856a244
Farrel Franqois <farrel@franqois.id>
2018-12-24 12:35:28 +0700

    Improvisasi pada file agar tidak bisa di akses secara langsung

--- config/function.php
+++ config/function.php
@@ -1,4 +1,16 @@
 <?php
+if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+  /* Dibawah ini adalah sebuah perintah agar mengubah kode status (Status Code) menjadi 403, saat file di akses secara langsung*/
+  if (!function_exists('http_response_code')) {
+    header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
+  } else {
+    http_response_code(403);
+  }
+  
+  /* Dibawah ini merupakan outputnya */
+  die("Anda dilarang mengaksesnya secara langsung!");
+
+} else {
  error_reporting(0);
  function autonumber($tabel, $kolom, $lebar=0, $awalan='') {
    include 'koneksi.php';
@@ -18,4 +30,5 @@
    }
    return $angka;
  }
+}
 ?>
--- errors/403.php
+++ errors/403.php
@@ -1,4 +1,16 @@
 <?php
+if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+  /* Dibawah ini adalah sebuah perintah agar mengubah kode status (Status Code) menjadi 403, saat file di akses secara langsung*/
+  if (!function_exists('http_response_code')) {
+    header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
+  } else {
+    http_response_code(403);
+  }
+  
+  /* Dibawah ini merupakan outputnya */
+  die("Anda dilarang mengaksesnya secara langsung!");
+
+} else {
   if (!function_exists('http_response_code')) {
     header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
   } else {
@@ -18,3 +30,4 @@
     <p>Mohon maaf, anda dilarang untuk mengakses halaman ini, silahkan <a href="javascript:window.history.back(-1);">kembali</a> !</p>
   </body>
 </html>
+<?php } ?>
