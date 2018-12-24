d257d576adc2a0e920f34463147db3fa73292d34
Farrel Franqois <farrel@franqois.id>
2018-12-24 12:26:43 +0700

    Improvisasi dengan Memanfaatkan Halaman Kesalahan

--- config/index.php
+++ config/index.php
@@ -1,7 +1,1 @@
-<?php
-  if (!function_exists('http_response_code')) {
-    header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden"); 
-  } else {
-    http_response_code(403);
-  }
-?>
+<?php require "../errors/403.php"; ?>
